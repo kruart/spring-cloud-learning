bbb66d0f8b34cb0b606d8e187fe83dc96e3ee17b
arthur.krukowski <weoz@ukr.net>
2019-05-02 13:43:04 +0300

    hard coded limits-service controller

---
+++ limits-service/src/main/java/com/cloudlearn/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,14 @@
+package com.cloudlearn.limitsservice;
+
+import com.cloudlearn.limitsservice.model.LimitConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitsFromConfigurations() {
+        return new LimitConfiguration(1000, 1);
+    }
+}
