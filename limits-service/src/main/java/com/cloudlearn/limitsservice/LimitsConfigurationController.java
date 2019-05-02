de09b5238d8591e9f83330a01e16a7d004303750
arthur.krukowski <weoz@ukr.net>
2019-05-02 16:52:20 +0300

    getting configuration from application.properies file

--- limits-service/src/main/java/com/cloudlearn/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/cloudlearn/limitsservice/LimitsConfigurationController.java
@@ -1,14 +1,19 @@
 package com.cloudlearn.limitsservice;
 
 import com.cloudlearn.limitsservice.model.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration conf;
+
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfigurations() {
-        return new LimitConfiguration(1000, 1);
+        return new LimitConfiguration(conf.getMaximum(), conf.getMinimum());
     }
 }
