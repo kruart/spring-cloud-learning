234124757af958690a9cfbf84e3149e875ca9ed2
arthur.krukowski <weoz@ukr.net>
2019-05-03 16:11:54 +0300

    invoking currency-exchange service from currency-conversion service

--- currency-conversion-service/src/main/java/com/cloudlearn/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/cloudlearn/currencyconversionservice/CurrencyConversionController.java
@@ -1,10 +1,14 @@
 package com.cloudlearn.currencyconversionservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
@@ -15,6 +19,14 @@ public class CurrencyConversionController {
             @PathVariable String to,
             @PathVariable BigDecimal quantity
     ) {
-        return new CurrencyConversionBean(1L, from, to, BigDecimal.ONE, quantity, quantity, 0);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversionBean.class, uriVariables);
+
+        CurrencyConversionBean resp = responseEntity.getBody();
+        return new CurrencyConversionBean(resp.getId(), from, to, resp.getConversionMultiple(),
+                quantity, quantity.multiply(resp.getConversionMultiple()), resp.getPort());
     }
 }
