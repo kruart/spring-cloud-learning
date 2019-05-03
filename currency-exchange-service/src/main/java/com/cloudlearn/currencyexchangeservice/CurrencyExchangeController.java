7a91b8b2a5a552cecd7e1c572b907a09d579b171
arthur.krukowski <weoz@ukr.net>
2019-05-03 13:53:30 +0300

    setting up dynamic port in the response

--- currency-exchange-service/src/main/java/com/cloudlearn/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/cloudlearn/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,16 +1,26 @@
 package com.cloudlearn.currencyexchangeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
+import static java.lang.Integer.parseInt;
+
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(27));
+        ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(27));
+        exchangeValue.setPort(parseInt(env.getProperty("local.server.port")));
+        return exchangeValue;
+
     }
 }
