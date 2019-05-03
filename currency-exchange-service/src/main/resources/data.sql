87e1862712a7c3918a143bf803a84001419cf757
arthur.krukowski <weoz@ukr.net>
2019-05-03 14:11:15 +0300

    configure jpa and initialized data

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+Insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10001, 'USD', 'UAH', 27, 0);
+
+Insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10002, 'EUR', 'UAH', 30, 0);
+
+Insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10003, 'AUD', 'UAH', 12, 0);
